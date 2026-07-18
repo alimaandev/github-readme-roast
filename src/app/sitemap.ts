34d637a0b1bdb9a36fed74eedd9966066d88dc8f
Ali Sher <alishermaan0319@gmail.com>
2026-07-18 10:26:52 +0500

    Fix sitemap URL and add sitemap reference to robots.txt

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,9 +1,13 @@
 import type { MetadataRoute } from "next";
 
+const BASE = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "https://github-readme-roast.vercel.app";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://readme-roast.vercel.app",
+      url: BASE,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
