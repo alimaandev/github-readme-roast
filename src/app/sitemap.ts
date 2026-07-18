7c1a94d0316de5d3b9fc459260ac645d3634d972
Ali Sher <alishermaan0319@gmail.com>
2026-07-18 10:34:45 +0500

    Update sitemap URL

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,7 +2,7 @@ import type { MetadataRoute } from "next";
 
 const BASE = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
-  : "https://github-readme-roast.vercel.app";
+  : "https://github-readme-roast-mu.vercel.app/";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
